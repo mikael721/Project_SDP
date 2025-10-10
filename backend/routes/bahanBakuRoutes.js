c3627a53e7aedee24f241b57c61ade398a200879
mikael721 <mikaelprajogo2810@gmail.com>
2025-10-10 12:41:56 +0700

    change route name

--- backend/routes/bahanBakuRoutes.js
+++ backend/routes/bahanBakuRoutes.js
@@ -4,7 +4,7 @@ const bahanBakuController = require("../controllers/bahanBakuController");
 
 router.get("/", bahanBakuController.getAllBahanBaku);
 router.get("/:id", bahanBakuController.getBahanBakuById);
-router.post("/", bahanBakuController.addBahanBaku);
+router.post("/new", bahanBakuController.addBahanBaku);
 router.put("/add", bahanBakuController.addBahanBakuJumlah);
 router.put("/subtract", bahanBakuController.subtractBahanBakuJumlah);
 router.delete("/:id", bahanBakuController.deleteBahanBaku);
