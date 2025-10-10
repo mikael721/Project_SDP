339e4516dbd634ca3d96ff54ee6f653fa8734c4e
mikael721 <mikaelprajogo2810@gmail.com>
2025-10-10 10:32:11 +0700

    instal cors + dotenv

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,15 +1,18 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config(); // Load environment variables
 
 const sequelize = new Sequelize(
-  "db_pengunjung", // database name
-  "root", // database username
-  "", // database password
+  process.env.DB_NAME, // database name
+  process.env.DB_USER, // database username
+  process.env.DB_PASSWORD, // database password
   {
-  host: "localhost",
-  dialect: "mysql", 
-});
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
-async function testDB() { // test database connection
+async function testDB() {
+  // test database connection
   try {
     await sequelize.authenticate();
     console.log("Database connected!");
