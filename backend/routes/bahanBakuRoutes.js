fbe156913344c86d6d896d647ad43d440fbebb48
mikael721 <mikaelprajogo2810@gmail.com>
2025-10-10 12:40:58 +0700

    fix error

--- backend/routes/bahanBakuRoutes.js
+++ backend/routes/bahanBakuRoutes.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const bahanBakuController = require("../controllers/bahanBakuController");
 
-// Routes for Bahan Baku
-router.get("/", bahanBakuController.getAllBahanBaku); // Get all Bahan Baku
-router.get("/:id", bahanBakuController.getBahanBakuById); // Get Bahan Baku by ID
-router.post("/", bahanBakuController.addBahanBaku); // Add a new Bahan Baku
-router.put("/add", bahanBakuController.addBahanBakuJumlah); // Add to Bahan Baku jumlah
-router.put("/subtract", bahanBakuController.subtractBahanBakuJumlah); // Subtract from Bahan Baku jumlah
-router.delete("/:id", bahanBakuController.deleteBahanBaku); // Delete Bahan Baku by ID
+router.get("/", bahanBakuController.getAllBahanBaku);
+router.get("/:id", bahanBakuController.getBahanBakuById);
+router.post("/", bahanBakuController.addBahanBaku);
+router.put("/add", bahanBakuController.addBahanBakuJumlah);
+router.put("/subtract", bahanBakuController.subtractBahanBakuJumlah);
+router.delete("/:id", bahanBakuController.deleteBahanBaku);
 
 module.exports = router;
