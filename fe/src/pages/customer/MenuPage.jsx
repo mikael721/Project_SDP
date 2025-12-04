26230ab8f09bbc0564ad957960cd579adb03998f
owen550 <omoses611@gmail.com>
2025-12-04 11:17:05 +0700

    Get Help Page

--- fe/src/pages/customer/MenuPage.jsx
+++ fe/src/pages/customer/MenuPage.jsx
@@ -8,6 +8,8 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import menuSlice from "../../slice + storage/menuSlice";
 import { AppShell, Button, Container, Group, Image } from "@mantine/core";
+import GambarBuLis from '../../asset/bu-Lis-Ask-NoBg.png'
+import { useSetState } from "@mantine/hooks";
 
 const MenuPage = () => {
   // === VARIABEL ===
@@ -23,6 +25,9 @@ const MenuPage = () => {
 
   // === USE STATE ===
   const [menu, setmenu] = useState([]);
+  const [showHelp, setshowHelp] = useState(false)
+  
+  const [showHelpDetail, setshowHelpDetail] = useState('none');
 
   // === FUNCTION ===
   const getMenu = async () => {
@@ -37,6 +42,75 @@ const MenuPage = () => {
     }
   };
 
+  const showHelpPanel = () => {
+    setshowHelp(!showHelp);
+  }
+
+  const renderDetailHelp = () => {
+    if(showHelpDetail == 'none'){ // default
+      return(
+        <div>
+          none
+        </div>
+      )
+    }
+    else if(showHelpDetail == 'pesan_menu'){
+      return(
+        <div>
+          pesan_menu
+        </div>
+      )
+    }
+    else{
+      return(
+        <div>
+          lihat_riwayat
+        </div>
+      )
+    }
+  }
+
+  const renderShowHelpPanel = () => {
+    if(showHelp){
+        console.log('Show halaman');
+        
+        return(
+        <div 
+          className="getHelp"
+        >
+          <div className="helpMain">
+            <div className="helpKuadran">
+              <div className="helpChatpanel panelInformasi">
+
+                {/* untuk chat nanti muncul di sini*/}
+                {renderDetailHelp()}
+              
+              </div>
+              <div className="">
+                <h3>Selamat Datang Di Halaman Bantuan Apa yang Bisa Kami Bantu ?</h3>
+                <button className="btnHelp" onClick={() => {
+                  setshowHelpDetail('pesan_menu');
+                }}>Cara Memesan Makanan</button> <br />
+                <button className="btnHelp" onClick={() => {
+                  setshowHelpDetail('lihat_riwayat');
+                }}>
+                  Cara Melihat Orderan</button> <br />
+                <button className="btnHelp" onClick={() => {
+                  setshowHelp(!showHelp);
+                  setshowHelpDetail('none');
+                }}>Cancle</button> <br />
+              </div>
+            </div>
+            <div className="helpKuadran setmidle">
+              <img src={GambarBuLis} alt="Sedang Memuat" className="setUkuranGambar"/>
+            </div>
+          </div>
+
+        </div>
+      )
+    }
+  }
+
   const goToCart = () => {
     if (menuTerpesan.length === 0) {
       window.alert("Anda Belum Memilih Menu");
@@ -46,67 +120,86 @@ const MenuPage = () => {
   };
 
   return (
-    <AppShell header={{ height: 70 }} padding="">
-      <AppShell.Header>
-        <Container
-          fluid
-          h="100%"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}>
-          <Group h="100%">
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <Image src={logo} alt="Logo" h={65} fit="contain" />
-            </Link>
-          </Group>
-          <Group style={{ display: "flex" }}>
-            <Button
-              size="lg"
-              radius="xl"
-              onClick={() => {
-                navigate(`/customer/history`);
-              }}
-              style={{
-                marginLeft: "auto",
-                color: "white",
-                border: `1px solid white`,
-                backgroundColor: "blue",
-              }}>
-              History
-            </Button>
-            <Button
-              size="lg"
-              radius="xl"
-              onClick={goToCart}
-              style={{
-                marginLeft: "10px",
-                color: "white",
-                border: `1px solid white`,
-                backgroundColor: "#CC0000",
-              }}>
-              Cart
-            </Button>
-          </Group>
-        </Container>
-      </AppShell.Header>
-      <AppShell.Main>
-        <div className="menuList">
-          {menu
-            .filter((d) => d.menu_status_aktif === 1)
-            .map((d) => (
-              <CardMenu
-                key={d.menu_id}
-                img={d.menu_gambar}
-                harga={d.menu_harga}
-                nama={d.menu_nama}
-                id={d.menu_id}
-              />
-            ))}
-        </div>
-      </AppShell.Main>
-    </AppShell>
+    <>
+      {renderShowHelpPanel()}
+      <AppShell header={{ height: 70 }} padding="">
+        <AppShell.Header>
+          <Container
+            fluid
+            h="100%"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}>
+            <Group h="100%">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Image src={logo} alt="Logo" h={65} fit="contain" />
+              </Link>
+            </Group>
+            <Group style={{ display: "flex" }}>
+              
+              <Button
+                size="lg"
+                radius="xl"
+                onClick={() => {
+                  showHelpPanel();
+                }}
+                style={{
+                  marginLeft: "auto",
+                  color: "white",
+                  border: `1px solid white`,
+                  backgroundColor: "lime",
+                }}>
+                Cara Memesan
+              </Button>
+
+              <Button
+                size="lg"
+                radius="xl"
+                onClick={() => {
+                  navigate(`/customer/history`);
+                }}
+                style={{
+                  marginLeft: "auto",
+                  color: "white",
+                  border: `1px solid white`,
+                  backgroundColor: "blue",
+                }}>
+                History
+              </Button>
+              <Button
+                size="lg"
+                radius="xl"
+                onClick={goToCart}
+                style={{
+                  marginLeft: "10px",
+                  color: "white",
+                  border: `1px solid white`,
+                  backgroundColor: "#CC0000",
+                }}>
+                Cart
+              </Button>
+            </Group>
+          </Container>
+        </AppShell.Header>
+        <AppShell.Main>
+          <div className="menuList">
+            {menu
+              .filter((d) => d.menu_status_aktif === 1)
+              .map((d) => (
+                <CardMenu
+                  key={d.menu_id}
+                  img={d.menu_gambar}
+                  harga={d.menu_harga}
+                  nama={d.menu_nama}
+                  id={d.menu_id}
+                />
+              ))}
+          </div>
+        </AppShell.Main>
+      </AppShell>
+    </>
   );
 };
 
